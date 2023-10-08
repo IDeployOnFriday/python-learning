4b7e25c43ebef9886d287e0a257a50d4a00ad137
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-10-08 16:40:00 +0100

    python first class strings and ints

--- main.py
+++ main.py
@@ -4,13 +4,25 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_func(message):
+    print(type(message), message)
+
+
+def learn_strings():
+    my_string = "this is a simple string"
+    my_string += " and i added stuff"
+    print_func(my_string)
+
+
+def learn_ints():
+    my_int = 1
+    my_int += 2
+    print_func(my_int)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    learn_strings()
+    learn_ints()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
