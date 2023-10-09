fa1751c2edde442ad156f295448a3241c7706724
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-10-09 08:12:57 +0100

    basic string stuff

--- main.py
+++ main.py
@@ -9,20 +9,37 @@ def print_func(message):
 
 
 def learn_strings():
-    my_string = "this is a simple string"
+# join strings 
+    my_string = "this is a " + "simple string"
     my_string += " and i added stuff"
     print_func(my_string)
 
+#multiply strings 
+    print("ha" * 4)
+
+#find occurence 
+    print(my_string.find('a'))
+
+# change case 
+    print(my_string.upper()) 
+    print(my_string.lower()) 
 
 def learn_ints():
     my_int = 1
     my_int += 2
     print_func(my_int)
 
+def learn_multiline_strings():
+    multi_string = '''
+                    line 1
+                    line 2
+                    line 3 
+                    '''
+    print_func(multi_string)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     learn_strings()
-    learn_ints()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
