1d5fd92b2ef1cd30e76c7b8215564a30cfcb657f
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-10-15 19:05:54 +0100

    futher lists

--- lists.py
+++ lists.py
@@ -1,15 +1,30 @@
 
 
 def foo() : 
+    # create an empty list 
     users = []
+
+    # add elements to a list 
     users.append('kevin')
     users.append('bob')
     users.append('alice')
     assert users == ['kevin', 'bob', 'alice'], f"Expected `users` to be ['kevin', 'bob', 'alice'] but got: {repr(users)}"
 
+    # delete value from a list
     del users[1]
     assert users == ['kevin', 'alice'], f"Expected `users` to be ['kevin', 'alice'] but got: {repr(users)}"
 
+    # reverse a list 
     rev_users = list(reversed(users))
     assert rev_users == ['alice', 'kevin'], f"Expected `rev_users` to be ['alice', 'kevin'] but got: {repr(rev_users)}"
+
+    # add an element to the list in position 1
+    users.insert(1, 'melody')
+    assert users == ['kevin', 'melody', 'alice'], f"Expected `users` to be ['kevin', 'melody', 'alice'] but got: {repr(users)}"
+
+    #add extra elelements to a list 
+    users += ['andy', 'wanda', 'jim'] 
+    assert users == ['kevin', 'melody', 'alice', 'andy', 'wanda', 'jim'], f"Expected `users` to be ['kevin', 'melody', 'alice', 'andy', 'wanda', 'jim'] but got: {repr(users)}"
+
+    print('list lesson completed')
 
