860145643e623ef1e7b0f2b74ff34b5064ba3d3c
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-10-20 08:03:07 +0100

    adding loop to fizz buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,12 @@
 def fizz_buzz_game(): 
-    value = int(input("enter an integer : "))
-    if value % 5 == 0 and value % 3 == 0:
-        print("FizzBuzz") 
-    elif value % 3 ==0:
-        print("Fizz") 
-    elif value % 5 ==0:
-        print("buzz")
-    else: 
-        print(value)
+    upper_number=int(input("how many numbers should we processs: "))
+    for value in range(1, upper_number + 1):
+       
+        if value % 5 == 0 and value % 3 == 0:
+            print("FizzBuzz") 
+        elif value % 3 ==0:
+            print("Fizz") 
+        elif value % 5 ==0:
+            print("buzz")
+        else: 
+            print(value)
