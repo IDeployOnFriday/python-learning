652976f51addf8466d754d1ccc87d275fa3f587e
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-10-21 19:25:39 +0100

    more strings

--- string_info.py
+++ string_info.py
@@ -29,5 +29,15 @@ def split_names(name):
         'last_name': last_name,
     }
 
+def is_palindrome(item): 
+    item = str(item)
+    return item == item[::1]
+
+def build_list(item, count=1): 
+    items = []
+    for _ in range(count): 
+        items.append(item)
+    return items
+
 
  
